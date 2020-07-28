58fba7c7c433149016f10710c1f204e3c505c048
Vitor Hugo Pereira Ribeiro <vitorhugopereiraribeiro@gmail.com>
2020-07-28 18:54:46 -0400

    Redic_Poc Project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RedisPOC_NetCore31
+
+A simple Redis example with .NET Core 3.1
---
+++ Redis_POC_API/Controllers/CacheController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace Redis_POC_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        private IDatabase _database;
+
+        public CacheController(IDatabase database)
+        {
+            _database = database;
+        }
+        
+        [HttpGet]
+        [Route("dados/{key}")]
+        public string ObterValor(string key)
+        {
+            return _database.StringGet(key);
+        }
+
+        [HttpPost]
+        [Route("dados")]
+        public void AtribuirValor([FromBody] KeyValuePair<string, string> keyValue)
+        {
+            _database.StringSet(keyValue.Key, keyValue.Value);
+        }
+        
+    }
+}
